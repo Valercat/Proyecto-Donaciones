66126b778a65f2abf97ed9f2852b2a8be60d8f88
AnaSalguero <164227583+AnaSalguero@users.noreply.github.com>
2025-11-26 21:41:34 -0600

    fix: Arreglar inserción de script

--- ScriptDonaciones_ONG.sql
+++ ScriptDonaciones_ONG.sql
@@ -363,6 +363,58 @@ FROM sys.database_files;
 	INSERT INTO recursos (nombre, cantidad)
 	VALUES ('nombre', 1111)
 
+--Creamos auditorias--
+
+--hacemos una auditoria a nivel de servidor
+USE master;
+
+CREATE SERVER AUDIT Audit_Donaciones
+TO FILE (FILEPATH = 'C:\auditoria\',
+MAXSIZE = 50 MB,MAX_FILES = 100)
+WITH (QUEUE_DELAY = 1000,
+ON_FAILURE = CONTINUE);
+GO
+ALTER SERVER AUDIT Audit_Donaciones WITH (STATE = ON);
+
+
+--generamos una auditoria a cada apartado con informacion considerada sensible
+USE Donaciones_ONG;
+
+CREATE DATABASE AUDIT SPECIFICATION Audit_DonacionesONG
+FOR SERVER AUDIT Audit_Donaciones
+
+    --Para esquema de donaciones
+    ADD (INSERT ON SCHEMA::donaciones BY PUBLIC),
+    ADD (UPDATE ON SCHEMA::donaciones BY PUBLIC),
+    ADD (DELETE ON SCHEMA::donaciones BY PUBLIC),
+
+    -- Para el esquema de personas
+    ADD (INSERT ON SCHEMA::personas BY PUBLIC),
+    ADD (UPDATE ON SCHEMA::personas BY PUBLIC),
+    ADD (DELETE ON SCHEMA::personas BY PUBLIC),
+
+    --Para el esquema de Aporte
+    ADD (INSERT ON SCHEMA::Aporte BY PUBLIC),
+    ADD (UPDATE ON SCHEMA::Aporte BY PUBLIC),
+    ADD (DELETE ON SCHEMA::Aporte BY PUBLIC),
+
+    -- Para esquema proyecto
+    ADD (INSERT ON SCHEMA::proyectos BY PUBLIC),
+    ADD (UPDATE ON SCHEMA::proyectos BY PUBLIC),
+    ADD (DELETE ON SCHEMA::proyectos BY PUBLIC),
+
+    -- Para esquema eventos
+    ADD (INSERT ON SCHEMA::eventos BY PUBLIC),
+    ADD (UPDATE ON SCHEMA::eventos BY PUBLIC),
+    ADD (DELETE ON SCHEMA::eventos BY PUBLIC)
+    WITH (STATE = ON);
+
+GO;
+--Comprobación de auditorias
+SELECT * 
+FROM sys.fn_get_audit_file('C:\auditoria\*', DEFAULT, DEFAULT);
+
+
 --ANDREA
 --Se habilitó la importación de datos provenientes de archivos 
 --CSV mediante el asistente de SQL Server y el comando 
